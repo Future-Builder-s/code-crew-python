cacd2f51f5704506170ed249cc606c199bdef3f5
Satyam <2000locomail@gmail.com>
2026-09-20 21:49:26 +0530

    Add personal introduction script with user information

---
+++ Satyam Dwivedi/Practices/01_personal_introduction.py
@@ -0,0 +1,7 @@
+# Store personal information in variables
+
+Name = "Satyam Dwivedi"
+Age = 18
+Goal = "AI Engineer"
+Country = "India"
+print(f"My name is {Name},\n I am {Age} years old\n from {Country}, \nand my goal is to become a {Goal}")
